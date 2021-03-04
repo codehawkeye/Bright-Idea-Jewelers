5b473c92c1665b79a1566c05128ec48eeb15ae88
Mike Carter <elite.mikecarter@gmail.com>
2021-03-04 10:20:05 -0500

    created product js

--- client/src/utils/App.js
+++ client/src/utils/App.js
@@ -1,28 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-// import { StoreProvider } from "./utils/GlobalState";
-import FavoritesList from "./pages/FavoritesList";
+import Wrapper from "./Wrapper";
+import Header from "./Header";
+import Main from "./Main";
+
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <StoreProvider>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/favorites" component={FavoritesList} />
-            <Route exact path="/posts/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
-        </StoreProvider>
-      </div>
-    </Router>
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
   );
 }
 
